49749336f9e2af88e13755621521910ee86b44d5
Mauri <ramirezgonzalomauricio@gmail.com>
2022-10-21 16:56:47 -0300

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Mauri on 21/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
